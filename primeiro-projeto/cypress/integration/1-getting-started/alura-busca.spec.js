a22c0ff8c63f6d6d90b0a20333d04c68f80d4f3a
Rafael Almeida <rafael13almeida@gmail.com>
2023-05-06 11:48:30 -0300

    Melhorias no arquivo alura-busca.spec.js

--- primeiro-projeto/cypress/integration/1-getting-started/alura-busca.spec.js
+++ primeiro-projeto/cypress/integration/1-getting-started/alura-busca.spec.js
@@ -1,13 +1,18 @@
 describe('alura busca cursos', () => {
 
     beforeEach(() => {
+        //visite o site da aluta 
         cy.visit('https://www.alura.com.br');
     })
 
     it('buscar curso de php', () => {
+        //busca o campo de busca e escreva php
         cy.get('#header-barraBusca-form-campoBusca').type('php');
+        //busca a lupa e clique nela 
         cy.get('.header-barraBusca-form-submit').click();
-        cy.get(':nth-child(6) > .busca-resultado-link > .busca-resultado-container > .busca-resultado-nome')
-            .should('have.text', 'Formação Laravel: crie aplicações web em PHP');
+        //busca o nome neste campo 
+        cy.get('h4.busca-resultado-nome')
+            //verifica se tem o texto, Formação Laravel: crie aplicações web em PHP
+            .should('contain', 'Formação Laravel: crie aplicações web em PHP');
     })
 })
