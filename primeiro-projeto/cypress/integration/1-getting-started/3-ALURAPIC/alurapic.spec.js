7f6d908dce9cbbc44f7e4b22f85b30ade10513a5
Rafael Almeida <rafael13almeida@gmail.com>
2023-05-06 16:51:59 -0300

    Adicionando itens de teste no cadastro de usuário

--- primeiro-projeto/cypress/integration/1-getting-started/3-ALURAPIC/alurapic.spec.js
+++ primeiro-projeto/cypress/integration/1-getting-started/3-ALURAPIC/alurapic.spec.js
@@ -13,6 +13,40 @@ describe('Login e registro de usuarios alura pic', () => {
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
         cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
-        cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');   
+    })
+
+    it('verificar mensagem de email invalido', () => {
+        cy.contains('a', 'Register now').click();
+
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="email"]').type('Codorninha');
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+    })
+
+    it('verificar mensagem de full name invalido', () => {
+        cy.contains('a', 'Register now').click();
+
+        cy.get('input[formcontrolname="fullName"]').type('F');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible');
+    })
+
+    it('verificar mensagem de user name invalido', () => {
+        cy.contains('a', 'Register now').click();
+
+        cy.get('input[formcontrolname="userName"]').type('F');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible');
+        cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');
+    })
+
+    it('verificar mensagem de password invalido', () => {
+        cy.contains('a', 'Register now').click();
+
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
     })
 })
