2268966ba5c45fffce24ece8887fd7025ffaccd1
Rafael Almeida <rafael13almeida@gmail.com>
2023-05-06 09:57:12 -0300

    Teste dell

--- primeiro-projeto/cypress/integration/1-getting-started/alura-busca.spec.js
+++ primeiro-projeto/cypress/integration/1-getting-started/alura-busca.spec.js
@@ -1,12 +1,16 @@
 describe('alura busca cursos', () => {
 
     beforeEach(() => {
+        //busca o site da alura
         cy.visit('https://www.alura.com.br');
     })
 
     it('buscar curso de php', () => {
+        //busca o input de busca e escreve 'php'
         cy.get('#header-barraBusca-form-campoBusca').type('php');
+        //busca o input de lupa e clica
         cy.get('.header-barraBusca-form-submit').click();
+        //busca o texto e verifica
         cy.get(':nth-child(6) > .busca-resultado-link > .busca-resultado-container > .busca-resultado-nome')
             .should('have.text', 'Formação Laravel: crie aplicações web em PHP');
     })
