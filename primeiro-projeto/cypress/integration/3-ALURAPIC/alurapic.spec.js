2a1db8ceb14f071d7c52817e8447807a7933558f
Rafael Almeida <rafael13almeida@gmail.com>
2023-05-08 15:16:25 -0300

    Adicionando o arquivo usuarios.json para massa de testes

--- primeiro-projeto/cypress/integration/3-ALURAPIC/alurapic.spec.js
+++ primeiro-projeto/cypress/integration/3-ALURAPIC/alurapic.spec.js
@@ -50,15 +50,36 @@ describe('Login e registro de usuarios alura pic', () => {
         cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
     })
 
-    it.only('fazer login com usuario valido', () => {
+    it('fazer login com usuario valido', () => {
         cy.login('flavio', 123);
         cy.contains('a', '(Logout)').should('be.visible');
     })
 
-    it.only('fazer login com usuario valido', () => {
+    it('fazer login com usuario invalido', () => {
         cy.login('Jack', 1234);
         cy.on('window:alert', str => {
             expect(str).to.equal('Invalid user name or password')
         })
     })
+
+    // it('criar um novo usuario', () => {
+    //     cy.contains('a', 'Register now').click();
+    //     cy.cadastro('Fael@mail', 'Fael', 'fael', 1234);
+    //     cy.contains('button', 'Register').click();
+    // })
+
+    const usuarios = require('../../fixtures/usuarios.json');
+    usuarios.forEach(usuario => {
+
+        it.only(`registra novo usuario ${usuario.userName}`, () => {
+            cy.contains('a', 'Register now').click();
+            cy.contains('button', 'Register').click();
+            cy.get('input[formcontrolname="email"]').type(usuario.email);
+            cy.get('input[formcontrolname="fullName"]').type(usuario.fullName);
+            cy.get('input[formcontrolname="userName"]').type(usuario.userName);
+            cy.get('input[formcontrolname="password"]').type(usuario.password);
+            cy.contains('button', 'Register');
+        })
+    })
+    
 })
--- primeiro-projeto/cypress/support/gui_commands.js
+++ primeiro-projeto/cypress/support/gui_commands.js
@@ -3,3 +3,12 @@ Cypress.Commands.add('login', (nome, senha) => {
     cy.get('input[formcontrolname="password"]').type(senha);
     cy.get('button[type="submit"]').click();
 })
+
+Cypress.Commands.add('cadastro', (email, fullName, user,  password) => {
+    
+    cy.get('input[formcontrolname="email"]').type(email);
+    cy.get('input[formcontrolname="fullName"]').type(fullName);
+    cy.get('input[formcontrolname="userName"]').type(user);
+    cy.get('input[formcontrolname="password"]').type(password);
+    
+})
