8650d92c19de258703ca3c39cb6345158b86db02
Rafael Almeida <rafael13almeida@gmail.com>
2023-05-10 16:19:41 -0300

    Adicionando comentários para entendimento sobre a função intercept

--- primeiro-projeto/cypress/integration/3-ALURAPIC/login.spec.js
+++ primeiro-projeto/cypress/integration/3-ALURAPIC/login.spec.js
@@ -2,6 +2,10 @@ describe('Login de usuarios alura pic', () => {
 
     beforeEach(() => {
         cy.visit('https://alura-fotos.herokuapp.com/#/home');
+
+        // cy.intercept('POST','https://apialurapic.herokuapp.com/user/login', {
+        //     statusCode: 400
+        // }).as('stubPost')
     })
 
     it('fazer login com usuario invalido', () => {
@@ -13,6 +17,7 @@ describe('Login de usuarios alura pic', () => {
 
     it('fazer login com usuario valido', () => {
         cy.login(Cypress.env('userName'), Cypress.env('password'));
+        // cy.wait('@stubPost');
         cy.contains('a', '(Logout)').should('be.visible');
     });
 })
